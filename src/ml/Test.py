e6473a313faccafd83930c71cdab00c4d7ec8070
yupeng <ypyupeng@icloud.com>
2016-05-29 23:26:22 +0800

    k 临近算法

--- src/ml/Test.py
+++ src/ml/Test.py
@@ -1,12 +1,20 @@
-from numpy import *
+import kNN
+import numpy
+import matplotlib
+import matplotlib.pyplot as plt
 
-randMat = mat(random.rand(4,4))
+print kNN.classify0([0, 0], numpy.array([[1, 0], [2, 1]]), ['A', 'B'], 1)
 
-invRandMat = randMat.I
-# print invRandMat
-# print randMat
-# print randMat*invRandMat
-myEye=randMat*invRandMat
-print myEye
-print myEye-eye(4)
-print eye(4)
+datingDataMat, datingLabels = kNN.file2matrix('test2.txt')
+# datingDataMat = numpy.zeros((3,3))
+# datingDataMat[2,:] = [2,1,0]
+# print datingDataMat ,datingDataMat[:,2]
+print datingLabels
+print 15.0*numpy.array(datingLabels)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2],15.0*numpy.array(datingLabels),15.0*numpy.array(datingLabels))
+plt.show()
+
+# ax.scatter([2,3,1],[3,1,2])
